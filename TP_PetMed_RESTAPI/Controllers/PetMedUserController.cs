bac363074c27f37c6f7ea9736b8fb07018cbf41f
Joe Michel <tun69277@temple.edu>
2022-12-03 11:18:11 -0500

    -UserController changes

--- TP_PetMed_RESTAPI/Controllers/PetMedUserController.cs
+++ TP_PetMed_RESTAPI/Controllers/PetMedUserController.cs
@@ -10,24 +10,12 @@ using PetMedLibrary;
 
 namespace TP_PetMed_RESTAPI.Controllers
 {
-    [Route("PetMed/[controller]")]
-    [ApiController]
+    [Route("PetMedUser/[controller]")]
     public class PetMedUserController : ControllerBase
     {
         User request = new User();
 
-        //
-        [HttpGet]
-        [HttpGet("GetUser/")]
-        public bool GetUser()
-        {
-            
-            //request.Email = ;
-            //request.Password = ;
-            return request.GetUser();
-        }
-
-        [HttpGet("GetUser/{email}. {password}")]
+        [HttpGet("GetUser/{email}/{password}")] //ask about this
         public bool GetUser(string email, string password)
         {
             request.Email = email;
@@ -41,21 +29,9 @@ namespace TP_PetMed_RESTAPI.Controllers
             return false;
         }
 
-        [HttpPost("CreateUser/{user}")]
-        public User CreaeteUser([FromBody]User theUser)
-        {
-            User newUser = new User();
-            newUser.Name = theUser.Name;
-            newUser.Email = theUser.Email;
-            newUser.Password = theUser.Password;
-            newUser.Address = theUser.Address;
-            newUser.PhoneNumber = theUser.PhoneNumber;
 
-            return newUser;
-        }
-
-        [HttpPost("CreateUser/")]
-        public User CreaeteUser([FromBody] string name, string email, string password, string address, string phoneNumber)
+        [HttpPost("CreateUser/")]//add in the attribute names
+        public User CreateUser([FromBody] string name, string email, string password, string address, string phoneNumber)
         {
             User newUser = new User();
             newUser.Name = name;
